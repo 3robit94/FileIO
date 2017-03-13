7024165cee903cbc9ed48c8fcd59fb346f5f9ec6
Timothy Robinson <Timothy Robinson>
2017-03-13 15:46:01 +0000

    Initial commit

---
+++ app/src/main/java/com/example/a3robit94/fileio/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.a3robit94.fileio;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
